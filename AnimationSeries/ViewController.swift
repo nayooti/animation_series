7db2d63a851ce9e61fe703d686d35173fc7b1cb7
nayooti <nayooti@gmail.com>
2021-01-14 21:39:05 +0100

    remove storyboard

--- AnimationSeries/ViewController.swift
+++ AnimationSeries/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  AnimationSeries
-//
-//  Created by Bastian van de Wetering on 14.01.21.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
